55a616fbda3d2f6060cf08997f24b0b2176e64c2
Kale Roberts <kroberts@netspend.com>
2020-02-05 13:15:03 -0600

    Remove scripts folder, exposed some webhooks I shouldnt have.

--- scripts/plain_text_png_script.sh
+++
@@ -1,7 +0,0 @@
-set max_images to 4 as number
-set slack_url to "https://hooks.slack.com/services/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" as text
-
-set json to "{\"blocks\": [{\"type\": \"section\", \"text\": {\"type\": \"mrkdwn\", \"text\": \"It is Jason O Clock\"}},{\"type\": \"image\",\"image_url\": \"https://raw.githubusercontent.com/KaleRoberts/sillyStuff/master/jason" & ((random number from 1 to max_images) as text) & ".png\",\"alt_text\": \"Its Jason O Clock!\"}]}" as text
-
-set curl_command to "curl -v -H 'Content-Type: application/json' --data '" & json & "' --proxy http://localhost:9999 " & slack_url
-do shell script curl_command
